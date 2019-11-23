a5e2a3efa715d215266d4a0f96882ae9e93d1a59
jacopo <jacopok@gmail.com>
2019-11-23 11:28:37 +0100

    Added figure

--- ap_first_semester/gr_exercises/python/sheet7.py
+++ ap_first_semester/gr_exercises/python/sheet7.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
+from matplotlib import rc
+rc('text', usetex=True)
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text.latex', preamble=r'''\usepackage{amsmath}
+          \usepackage{physics}
+          \usepackage{siunitx}
+          ''')
 
 R = np.linspace(1, 5, num=200)
 
@@ -8,6 +15,7 @@ f = lambda x: x ** (-2) - x ** (-3)
 
 fig = plt.figure(1)
 plt.plot(R, f(R))
-plt.xlabel('R = r/r_s')
+plt.xlabel('$R = r/r_s$')
 plt.grid()
 plt.savefig(fname="../figures/photon_effective_potential.pdf", format="pdf")
+plt.close()

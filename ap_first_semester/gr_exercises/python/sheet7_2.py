2077faacaa3abdd3256123a12706e9562b0a2368
jacopo <jacopok@gmail.com>
2019-11-25 13:43:06 +0100

    fixed main, updated exercise

---
+++ ap_first_semester/gr_exercises/python/sheet7_2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+from matplotlib import rc
+rc('text', usetex=True)
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text.latex', preamble=r'''\usepackage{amsmath}
+\usepackage{physics}
+\usepackage{siunitx}
+''')
+R = np.linspace(1, 1.5, num=200)
+f = lambda x: np.arcsin(np.sqrt(27/4/x**2 * (1 - 1/x))) / (np.pi/2)
+fig = plt.figure(1)
+plt.plot(R, f(R))
+plt.grid()
+plt.show()
+# plt.close()

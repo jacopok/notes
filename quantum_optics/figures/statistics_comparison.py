d865b484aa71d88bebc68491b24e76b732ede444
jacopo <jacopok@gmail.com>
2020-01-25 13:04:57 +0100

    Made statistics comparison plotter

--- quantum_optics/figures/statistics_comparison.py
+++ quantum_optics/figures/statistics_comparison.py
@@ -4,10 +4,10 @@ from astropy.visualization import astropy_mpl_style
 from scipy.special import factorial
 plt.style.use(astropy_mpl_style)
 
-def BE(n, n_bar):
+def thermal(n, n_bar):
   return ((n_bar/(1+nbar)) ** n / (1 + n_bar))
   
-def thermal(n, n_bar):
+def coherent(n, n_bar):
   return (np.exp(-n_bar) * n_bar ** n / factorial(n))
   
 ns = np.arange(0, 40)
@@ -16,7 +16,7 @@ width = 0.35
 alpha = .8
 
 for nbar in nbars:
-  plt.bar(ns, BE(ns, nbar), label='BE ' + str(nbar), alpha = alpha, width=width)
+  plt.bar(ns, coherent(ns, nbar), label='coherent ' + str(nbar), alpha = alpha, width=width)
   plt.bar(ns+width, thermal(ns, nbar), label='thermal ' + str(nbar), alpha = alpha, width=width)
 plt.legend()
 plt.show()

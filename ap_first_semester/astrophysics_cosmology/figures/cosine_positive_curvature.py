c98712f3c1a4113a340fedcc035b5120505f4c7e
jacopo <jacopok@gmail.com>
2020-09-28 13:16:10 +0200

    Added a figure

--- ap_first_semester/astrophysics_cosmology/figures/cosine_positive_curvature.py
+++ ap_first_semester/astrophysics_cosmology/figures/cosine_positive_curvature.py
@@ -15,10 +15,19 @@ from basic_units import cos,sin, radians
 theta_r = np.linspace(0, 2 * np.pi, num=1000)
 theta = np.array([x * radians for x in theta_r])
 
-plt.plot(theta, (1 - cos(theta)) / 2, label='Scale factor $a$')
-plt.plot(theta, (theta_r - sin(theta)) / 2, label='Time $t$')
+a = (1 - cos(theta)) / 2
+t = (theta_r - sin(theta)) / 2
+
+plt.plot(theta, a, label='Scale factor $a$')
+plt.plot(theta, t, label='Time $t$')
 plt.xlabel('Angle parameter $\\theta$')
 plt.ylabel('Normalized scale factor $a / \\widetilde{a}_0$ or time $t / \\widetilde{t}_0$')
 plt.legend()
 plt.savefig('positive_curvature_a.pdf', format = 'pdf')
 # plt.show()
+plt.close()
+
+plt.plot(t, a)
+plt.xlabel("Time $t / \\widetilde{t}_0$")
+plt.ylabel("Scale factor $a / \\widetilde{a}_0$")
+plt.savefig('positive_curvature_a_vs_t.pdf', format = 'pdf')

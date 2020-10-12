85f4c3f422dccdd582461864a45162cfa3e65f39
jacopo <jacopok@gmail.com>
2020-10-12 14:56:11 +0200

    New APC figure

--- ap_first_semester/astrophysics_cosmology/figures/beta_core_pressure.py
+++ ap_first_semester/astrophysics_cosmology/figures/beta_core_pressure.py
@@ -18,15 +18,18 @@ from astropy.cosmology import Planck15 as cosmo
 
 from astropy.visualization import quantity_support
 
-a = np.pi ** 2 * ac.k_B**2 / 15 / ac.hbar**3 / ac.c**3
+# a = np.pi ** 2 * ac.k_B**2 / 15 / ac.hbar**3 / ac.c**3
+a = ac.sigma_sb * 4  /ac.c
 
 beta = np.linspace(0, 1, num=200)
-mbar = .62
+mbar = .62 * ac.m_p
 
 M = np.sqrt(ac.G ** (-3 ) * (np.pi / 36)**(-1) * (3/a * (1-beta) / beta**4) * (ac.k_B / mbar)**(4))
 
 with quantity_support():
-    plt.semilogx(M.decompose(), beta)
-    plt.show()
+    plt.semilogx(M.to(u.M_sun), beta)
+    plt.xlabel('Mass [$M_{\\odot}$]')
+    plt.ylabel('Nonrelativistic pressure fraction $\\beta$')
+    plt.savefig('beta_star_core_pressure.pdf')
     
     # the units don't work!

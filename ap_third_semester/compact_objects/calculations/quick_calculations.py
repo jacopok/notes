e6dd79a79fda9faca9136ab3a230af0abc24198b
jacopo <jacopok@gmail.com>
2021-02-23 20:22:44 +0100

    COA updates

--- ap_third_semester/compact_objects/calculations/quick_calculations.py
+++ ap_third_semester/compact_objects/calculations/quick_calculations.py
@@ -3,4 +3,17 @@
 import numpy as np
 
 print(1 - np.sqrt(8 / 9))
-print(1/12)
+print(1 / 12)
+
+#%%
+
+theta = np.linspace(0, 2* np.pi)
+Cs = np.linspace(0, 2, num=10)
+
+ax = plt.subplot(projection='polar')
+ax.set_theta_zero_location('N')
+
+for C in Cs:
+    ax.plot(theta, C * np.sin(theta)**2)
+
+# %%

237a1e1127851f2bc118f923339988efda9230d7
jacopo <jacopok@gmail.com>
2021-11-16 15:19:41 +0100

    new rapidity figure

--- phd_courses/experimental_high_energy_astroparticle/figures/rapidity.py
+++ phd_courses/experimental_high_energy_astroparticle/figures/rapidity.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 NUM = int(1e4)
 
-def pseudo_rapidity(theta):
+def pseudo_rapidity(theta: np.ndarray):
     return - np.log(np.arctan(theta / 2))
 
 def rapidity():
-    thetas = np.linspace(0, np.pi, num=NUM)    
-    plt.plot(thetas, )
+    thetas = np.linspace(0, np.pi, num=NUM)
+    plt.plot(thetas, pseudo_rapidity(thetas))
+    plt.grid('on')
 
 if __name__ == "__main__":
     from make_all_figures import plot_and_save

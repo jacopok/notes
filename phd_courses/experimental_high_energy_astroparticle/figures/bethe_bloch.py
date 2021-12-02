2e75e6e205132971f8d12762ecb35d671f3668d7
jacopo <jacopok@gmail.com>
2021-12-02 18:39:15 +0100

    finalized the figure for the cosmic ray diffusion

--- phd_courses/experimental_high_energy_astroparticle/figures/bethe_bloch.py
+++ phd_courses/experimental_high_energy_astroparticle/figures/bethe_bloch.py
@@ -1,12 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.constants import codata2018 as ac
+import astropy.units as u
+
+Z = 10
+A = 20
+rho = 1 * u.g / u.cm**3
+
+n = (ac.N_A * Z * rho / A / ac.u).si
+z = 1
+
+
+def bethe_bloch_func(beta):
+    gamma = 1/ np.sqrt(1 - beta**2)
+    
+    prefactor = (
+        4 * np.pi 
+        / (ac.m_e * ac.c**2)
+        * n * z**2
+        / beta**2
+        * (ac.e**2
+            / (4 * np.pi * ac.eps0)
+        )**2
+    ).si
+    
+    return prefactor * 
 
 def bethe_bloch():
-    pass
+    beta = np.logspace(.1, 3)
+    
 
 def energy_loss():
     pass
-
 
 if __name__ == "__main__":
     from make_all_figures import plot_and_save
--- phd_courses/theoretical_high_energy_astroparticle/figures/cosmic_ray_diffusion.py
+++ phd_courses/theoretical_high_energy_astroparticle/figures/cosmic_ray_diffusion.py
@@ -4,10 +4,12 @@ from astropy.constants import codata2018 as ac
 import astropy.units as u
 from tqdm import tqdm
 from scipy.integrate import solve_ivp
-from matplotlib.colors import Normalize, LogNorm
+from matplotlib.colors import Normalize, LogNorm, FuncNorm
 from matplotlib.cm import ScalarMappable
 from astropy.visualization import quantity_support
 from matplotlib.ticker import MultipleLocator
+from scipy.integrate import trapezoid
+import cmasher as cmr
 
 gamma = 10
 v0 = ac.c * np.sqrt(1 - 1/gamma**2)
@@ -28,7 +30,6 @@ sign = 1
 phi0 = 0
 
 theta0 = 1.
-
 
 def derivative_func(k):
     o = omega_larmor.to(1/u.s).value
@@ -51,17 +52,33 @@ def derivative_func(k):
 
 k_resonance = (sign * omega_larmor / v0 / np.cos(theta0)).to(1 / u.AU)
 
-N_plotted = 400
+N_plotted = 200
+N_plotted_margins = 100
 
-logit_nums = np.linspace(-4, 4, num=N_plotted)
-between_zero_one = np.exp(logit_nums) / (1 + np.exp(logit_nums))
+lim_k_dex = 1
 
-k_range = 10**((between_zero_one * 2) - 1) * k_resonance
+def cubic(x, center=0, width=2*lim_k_dex):
+    return (width/2)**(-1.5) * (abs(x - center))**1.5 * np.sign(x-center)
+    
+def inverse_cubic(y, center=0, width=2*lim_k_dex):
+    return center + (abs(y))**(1/1.5) * (width/2) * np.sign(y)
 
-# k_range = np.logspace(-1, 1, num=N_plotted) * k_resonance
-# k_range = np.linspace(.8, 1.2, num=N_plotted) * k_resonance
+logs = cubic(np.linspace(-lim_k_dex, lim_k_dex, num=N_plotted))
 
-n_periods = 50
+k_range = 10**logs * k_resonance
+
+# k_range = np.concatenate(
+#     (
+#         np.logspace(-.8, -.3, num=N_plotted_margins),
+#         np.logspace(-.3, .3, num=N_plotted),
+#         np.logspace(+.3, .8, num=N_plotted_margins),
+#     )
+# ) * k_resonance
+
+
+# k_range = np.logspace(-.3, .3, num=N_plotted) * k_resonance
+
+n_periods = 100
 larmor_period = 2 * np.pi / omega_larmor
 
 global_pulsations = omega_larmor - k_range * v0 * np.cos(theta0)
@@ -83,22 +100,28 @@ def solver():
 
 def diffusion_over_time():
 
-    cmap = plt.get_cmap('Spectral')
-    norm = LogNorm(min(k_range).value, max(k_range.value))
+    cmap = cmr.iceburn
+    norm = LogNorm(k_range[0].value, k_range[-1].value)
+    # norm = FuncNorm((
+    #     lambda x: cubic(np.log10(x/k_resonance.value)),
+    #     lambda x: 10**inverse_cubic(x) * k_resonance.value), 
+    #     vmin=min(k_range).value, 
+    #     vmax=max(k_range).value
+    # )
 
     for sol in solver():
         plt.plot(
             sol.t/larmor_period.si.value,
             sol.y[0],
             c=cmap(norm(sol.k.value)),
-            alpha=2**(-1*np.log10(N_plotted)),
+            alpha=2**(-1.*np.log10(N_plotted)),
             lw=.5
         )
 
     mappable = ScalarMappable(norm=norm, cmap=cmap)
     cbar = plt.colorbar(mappable=mappable, label=f'k [{k_resonance.unit}]')
-    cbar.ax.hlines(k_resonance.value, 0, 2, color='black')
-    plt.gca().set_facecolor('black')
+    cbar.ax.hlines(k_resonance.value, 0, 2, color='white')
+    # plt.gca().set_facecolor('black')
 
     plt.xlabel('Time [Larmor periods]')
     plt.ylabel('Angle [radians]')
@@ -124,6 +147,29 @@ def final_point_variation():
     plt.title(f'$\\Delta \\theta$ over {n_periods} Larmor periods')
 
 
+# def final_point_integral():
+#     theta_final = [sol.y[0][-1] for sol in solver()]
+#     theta_diffs = (np.array(theta_final) - theta0)
+#     theta_vars = theta_diffs**2
+#     integral = trapezoid(y=theta_vars, x=k_range.value)
+#     th_value = (omega_larmor**2 * (delta_B / B)**2 * np.pi / v0 / np.cos(theta0) * t_span[1]).si
+#     print(th_value)
+#     print(integral)
+    
+    # # with quantity_support():
+    # plt.semilogx(k_range.value, theta_vars, lw=.8)
+    # plt.axvline(k_resonance.value, ls=':',
+    #             label='Resonance wavenumber', c='black')
+    # plt.xlabel(f'$k$ [{k_range.unit}]')
+    # plt.ylabel(f'$\\Delta \\theta$ [{theta_diffs.unit}]')
+
+    # plt.grid('on')
+
+    # plt.legend()
+    # plt.title(f'$\\Delta \\theta$ over {n_periods} Larmor periods')
+
+
+
 def integration_periods_plot():
     with quantity_support():
         plt.semilogx(k_range, integration_oom)
@@ -133,6 +179,7 @@ def integration_periods_plot():
 
 if __name__ == "__main__":
     from make_all_figures import plot_and_save
-    # plot_and_save(final_point_variation)
-    plot_and_save(diffusion_over_time)
+    plot_and_save(final_point_variation)
+    # plot_and_save(diffusion_over_time)
     # plot_and_save(integration_periods_plot)
+    # final_point_integral()

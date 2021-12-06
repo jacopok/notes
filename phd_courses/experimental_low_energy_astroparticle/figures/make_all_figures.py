2250bc03473bb88ed53fcc850764f725582409a4
jacopo <jacopok@gmail.com>
2021-12-06 18:45:58 +0100

    updated Juno flux picture

--- phd_courses/experimental_low_energy_astroparticle/figures/make_all_figures.py
+++ phd_courses/experimental_low_energy_astroparticle/figures/make_all_figures.py
@@ -1,5 +1,5 @@
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+from tqdm import tqdm # type: ignore
+import matplotlib.pyplot as plt # type: ignore 
 from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
@@ -8,14 +8,15 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
 \usepackage{siunitx}
 ''')
 rc('figure', dpi=150)
+from typing import Callable
 
 
-def plot_and_save(plotting_func):
+def plot_and_save(plotting_func : Callable):
     plotting_func()
     plt.savefig(str(plotting_func.__name__).split(sep='.')[0] + '.pdf', bbox_inches='tight', pad_inches = 0)
     plt.close()
 
 if __name__ == "__main__":
-    plotter_list = []
+    plotter_list : list[Callable] = []
     for plotting_func in tqdm(plotter_list):
         plot_and_save(plotting_func)
--- phd_courses/experimental_low_energy_astroparticle/figures/oscillations.py
+++ phd_courses/experimental_low_energy_astroparticle/figures/oscillations.py
@@ -1,45 +1,44 @@
-import numpy as np
-import nptyping as npt
-from typing import Any, Optional
-import matplotlib.pyplot as plt
-from astropy.constants import codata2018 as ac
-import astropy.units as u
-from astropy.visualization import quantity_support
+import numpy as np  # type: ignore
+import nptyping as npt # type: ignore
+from typing import Any, Optional, Tuple
+import matplotlib.pyplot as plt # type: ignore
+from astropy.constants import codata2018 as ac # type: ignore
+import astropy.units as u # type: ignore
+from astropy.visualization import quantity_support  # type: ignore
 
 neutrino_names = ['e', 'mu', 'tau']
 
+m_min = 0 * u.eV
+
 def osc_probability(
-        E_over_L: np.float64,
-        initial_flavour: int = 0,
-        final_flavour: int = 1,
-        masses: Optional[npt.NDArray[(3,), np.float64]] = None,
-        mixing_matrix: Optional[npt.NDArray[(3, 3), np.complex128]] = None,
+    E_over_L: npt.NDArray[float],
+    initial_flavour: int = 0,
+    final_flavour: int = 1,
+    ordering: str = 'normal',
     ):
     """
     Probability of oscillation
 
     Args:
-        E_over_L (np.float64): in eV**2
-        masses (npt.NDArray): [m1, m2, m3], in eV
-        mixing_matrix (npt.NDArray): [flavour, mass], where flavour is in (e, mu, tau)
+        E_over_L (float): in eV**2
         initial_flavour (int): number between 0=e, 1=mu, 2=tau
         final_flavour (int): number between 0=e, 1=mu, 2=tau
+        ordering (str): either normal or inverted
     """
-    
-    if masses is None and mixing_matrix is None:
-        masses, mixing_matrix = prepare_oscillation_parameters()
+
+    masses, mixing_matrix = prepare_oscillation_parameters(ordering)
 
     return abs(np.sum(
         np.conj(mixing_matrix[initial_flavour, :, np.newaxis]) *
         mixing_matrix[final_flavour, :, np.newaxis] *
-        np.exp(- 1j * masses[:, np.newaxis]**2 
-                    / E_over_L[np.newaxis, :] 
-                    / 2),
+        np.exp(- 1j * masses[:, np.newaxis]**2
+               / E_over_L[np.newaxis, :]
+               / 2),
         axis=0
     ))**2
 
 
-def matrix(theta_12, theta_13, theta_23, delta_CP):
+def matrix(theta_12: float, theta_13: float, theta_23: float, delta_CP: float):
 
     c12 = np.cos(theta_12)
     c13 = np.cos(theta_13)
@@ -51,124 +50,149 @@ def matrix(theta_12, theta_13, theta_23, delta_CP):
 
     return np.array([
         [
-            c12*c13, 
-            s12*c13, 
+            c12*c13,
+            s12*c13,
             s13*np.exp(-1j*delta_CP)
         ],
         [
             -s12*c23-c12*s23*s13*np.exp(1j*delta_CP),
-            c12*c23 - s12*s13*s23*np.exp(1j * delta_CP), 
+            c12*c23 - s12*s13*s23*np.exp(1j * delta_CP),
             s23*c13
-            ],
+        ],
         [
-            s12*s23 - c12*c23*s13*np.exp(1j*delta_CP), 
+            s12*s23 - c12*c23*s13*np.exp(1j*delta_CP),
             -c12*s23 - s12*c23*s13*np.exp(1j * delta_CP),
             c23*c13
         ]
     ])
 
-def osc_plot(E_over_L, op, initial, final):
+
+@u.quantity_input
+def osc_plot(
+    E_over_L,
+    # E_over_L: u.Quantity[u.MeV / u.km],
+    op: np.ndarray, 
+    initial: int, 
+    final: int
+    ):
 
     with quantity_support():
-        
-        plt.semilogx(E_over_L, op, label=f'Transition from {neutrino_names[initial]} to {neutrino_names[final]}')
 
-        # ax = plt.gca()
-        # ax2 = ax.twiny()
-        # ax2.semilogx(1 / E_over_L, op, alpha=.1)
-        # ax2.set_xlim(reversed(ax2.get_xlim()))
+        plt.semilogx(
+            E_over_L, op, label=f'Transition from {neutrino_names[initial]} to {neutrino_names[final]}')
 
-def flux_plot(L_over_E, op, flux):
+@u.quantity_input
+def flux_plot(
+    L_over_E,
+    # L_over_E: u.Quantity[u.km / u.MeV],
+    op: np.ndarray, 
+    flux: np.ndarray, 
+    **kwargs):
 
     with quantity_support():
-        plt.plot(L_over_E, op * flux) 
+        plt.plot(L_over_E, op * flux, **kwargs)
         plt.plot(L_over_E, flux, ls=':')
 
-def flux(E):
+
+def flux(E: np.ndarray):
     """ Parametrization from https://arxiv.org/abs/0807.3203, 
     where the energy is in MeV (?)
     """
     # E /= 1.2
-    
+
     # energy and momentum of the emitted positron
     Ep = E - 1.29333236  # Energy minus neutron-proton mass difference
     pp = np.sqrt(Ep**2 - 0.51099895**2)  # proton energy minus positron mass
-    
+
     return (
-        .58 * np.exp(.87  - .16*E  - .091*E**2)+
-        .30 * np.exp(.896 - .239*E - .0981*E**2)+
-        .07 * np.exp(.976 - .162*E - .079*E**2)+
-        .05 * np.exp(.793 - .08*E  - .1085*E**2)
-        ) * Ep * pp
+        .58 * np.exp(.87 - .16*E - .091*E**2) +
+        .30 * np.exp(.896 - .239*E - .0981*E**2) +
+        .07 * np.exp(.976 - .162*E - .079*E**2) +
+        .05 * np.exp(.793 - .08*E - .1085*E**2)
+    ) * Ep * pp * E**2
 
-def prepare_oscillation_parameters():
 
-    # in terms of the given values for sin**2 (theta)
-    # theta_12 = np.arcsin(np.sqrt(.31))
-    theta_12 = np.arcsin(np.sqrt(.32))
-    # theta_13 = np.arcsin(np.sqrt(.558))
-    theta_13 = np.arcsin(np.sqrt(.007))
-    # theta_23 = np.arcsin(np.sqrt(.02241))
-    theta_23 = np.arcsin(np.sqrt(.5))
-    delta_CP = (222 * u.degree).to(u.rad).value
-    
+def prepare_oscillation_parameters(ordering: str = 'normal') -> Tuple[npt.NDArray[(3, ), float], npt.NDArray[(3, 3), np.complex128]]:
+
+    # based on the values given in http://arxiv.org/abs/1507.05613
+
+    if ordering == 'normal':
+        theta_12 = np.arcsin(np.sqrt(.308))
+        theta_13 =  np.arcsin(np.sqrt(.1)) / 2
+        # theta_13 = np.arcsin(np.sqrt(.0234))
+        theta_23 = np.arcsin(np.sqrt(.437))
+        delta_m21_square = 7.54e-5 * u.eV**2
+        delta_m31_square = 2.47e-3 * u.eV**2
+        delta_CP = 1.39 * np.pi
+
+        m1 = m_min
+        m2 = np.sqrt(delta_m21_square + m1**2)
+        m3 = np.sqrt(delta_m31_square + m1**2)
+
+    elif ordering == 'inverted':
+        theta_12 = np.arcsin(np.sqrt(.308))
+        theta_13 = np.arcsin(np.sqrt(.0240))
+        theta_23 = np.arcsin(np.sqrt(.455))
+        delta_m21_square = 7.54e-5 * u.eV**2
+        delta_m31_square = -2.42e-3 * u.eV**2
+        delta_CP = 1.31 * np.pi
+
+        m3 = m_min
+        m1 = np.sqrt(m3**2 - delta_m31_square)
+        m2 = np.sqrt(delta_m21_square + m1**2)
+
+    else:
+        raise NotImplementedError(f'Ordering {ordering} not found')
+
     U = matrix(theta_12, theta_13, theta_23, delta_CP)
-    
-    m1 =  (2 * u.meV).to(u.eV)
-    
-    # delta_m21_square = (73.9 * u.meV**2).to(u.eV**2)
-    # delta_m32_square = (2449 * u.meV**2).to(u.eV**2)
-
-    delta_m21_square = 7.6e-5 * u.eV**2
-    delta_m32_square = 2.4e-3 * u.eV**2
-
-    
-    m2 = np.sqrt(delta_m21_square + m1**2)
-    m3 = np.sqrt(delta_m32_square + m2**2)
-
     masses = np.array([m1.value, m2.value, m3.value])
 
     return masses, U
+
 
 def oscillations():
 
     # parameters for Juno
     E_range = np.logspace(0, 4, num=2000) * u.MeV
     L = 60 * u.km
-    
+
     E_over_L_range = (E_range / L * ac.c * ac.hbar).to(u.eV**2).value
 
     initial = 0
-    rescaled_E_over_L = (E_over_L_range * u.eV**2 / ac.hbar / ac.c).to(u.MeV / u.km)
+    rescaled_E_over_L = (E_over_L_range * u.eV**2 /
+                         ac.hbar / ac.c).to(u.MeV / u.km)
 
     for final in range(3):
         op = osc_probability(E_over_L_range, initial, final)
         osc_plot(rescaled_E_over_L, op, initial, final)
-    
+
     plt.legend()
+
 
 def juno_flux():
 
-    E = 1 * u.MeV 
+    E = 1 * u.MeV
     L_range = np.linspace(5, 32, num=1000) * u.km
-    
+
     E_over_L_range = (E / L_range * ac.c * ac.hbar).to(u.eV**2).value
-    
-    op = osc_probability(E_over_L_range, 0, 0)
-    
-    rescaled_E_over_L = (E_over_L_range * u.eV**2 / ac.hbar / ac.c).to(u.MeV / u.km)
-    
+
+    op_normal = osc_probability(E_over_L_range, 0, 0, ordering='normal')
+    op_inverted = osc_probability(E_over_L_range, 0, 0, ordering='inverted')
+
+    rescaled_E_over_L = (E_over_L_range * u.eV**2 /
+                         ac.hbar / ac.c).to(u.MeV / u.km)
+
     f = flux((rescaled_E_over_L * 60 * u.km).to(u.MeV).value)
-    
+
     # op = np.convolve(op, np.ones(25)/25, mode='same')
 
-    flux_plot(1/rescaled_E_over_L, op, f)
+    flux_plot(1/rescaled_E_over_L, op_normal, f, label='normal')
+    flux_plot(1/rescaled_E_over_L, op_inverted, f, label='inverted')
     plt.ylabel('Flux [arbitrary units]')
-
+    plt.legend()
 
 if __name__ == "__main__":
 
     from make_all_figures import plot_and_save
     plot_and_save(oscillations)
     plot_and_save(juno_flux)
-    

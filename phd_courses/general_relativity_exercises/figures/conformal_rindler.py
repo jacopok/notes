9cc177c3c4a5ac3b75f62d24ac8f91256ae18f14
jacopok <jacopok@gmail.com>
2022-06-28 19:25:58 +0200

    MORE GR

--- phd_courses/general_relativity_exercises/figures/conformal_rindler.py
+++ phd_courses/general_relativity_exercises/figures/conformal_rindler.py
@@ -2,29 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import transforms
 
+cmap = plt.get_cmap('plasma')
+
 def conformal_trajectory():
-    lam = np.linspace(-5, 5, num=1000)
-    a = 1
-    t = np.sinh(a * lam) / a
-    r = np.cosh(a * lam) / a
-    
-    u = t - r
-    v = t + r
-    
-    U = np.arctan(u) 
-    V = np.arctan(v)
+    lam = np.linspace(-20, 20, num=1000)
     
     base = plt.gca().transData
     rot = transforms.Affine2D().rotate_deg(45)
     
-    plt.plot(U, V, transform = rot + base)
+    for a in [1/2, 1, 2]:
+        t = np.sinh(a * lam) / a
+        r = np.cosh(a * lam) / a
+        
+        u = t - r
+        v = t + r
+        
+        U = np.arctan(u)
+        V = np.arctan(v)
+        
+        plt.plot(V, U, transform = rot + base, label = f'a={a}', color=cmap(a/3))
     
-    UV_range = np.linspace(-np.pi/2, np.pi/2, num=1000)
-    plt.plot(UV_range, UV_range, transform = rot + base)
-    plt.plot(UV_range, np.pi/2 * np.ones_like(UV_range), transform = rot + base)
-    plt.plot(np.pi/2*np.ones_like(UV_range), UV_range, transform = rot + base)
-    plt.plot(UV_range, -np.pi/2 * np.ones_like(UV_range), transform = rot + base)
-    plt.plot(-np.pi/2*np.ones_like(UV_range), UV_range, transform = rot + base)
+    ph = np.pi/2
+    
+    UV_range = np.linspace(-ph, ph, num=1000)
+    kwargs = {
+        'transform': rot + base,
+        'color': 'black'
+    }
+    
+    kwargs_curve = {
+        'transform': rot + base,
+        'color': 'green'
+    }
+    
+    plt.plot(UV_range, UV_range, **kwargs, ls='--')
+    plt.plot(ph*np.ones_like(UV_range), UV_range, **kwargs)
+    plt.plot(UV_range, -ph * np.ones_like(UV_range), **kwargs)
+
+    t = np.linspace(- 100, 0, num=1000)
+    r = np.zeros_like(t)
+    u = t - r
+    v = t + r    
+    U = np.arctan(u)
+    V = np.arctan(v)    
+    plt.plot(V, U, **kwargs_curve)
+
+    t = np.linspace(0, 1, num=200)
+    r = np.sqrt(1 + t**2) - 1
+    u = t - r
+    v = t + r    
+    U = np.arctan(u)
+    V = np.arctan(v)
+    plt.plot(V, U, **kwargs_curve, ls=':')
+
+    t = np.linspace(1, 100, num=200)
+    r = t / np.sqrt(2) - 1/ np.sqrt(2) + np.sqrt(2) - 1
+    u = t - r
+    v = t + r    
+    U = np.arctan(u)
+    V = np.arctan(v)    
+    plt.plot(V, U, **kwargs_curve)
+
+
+    plt.text(ph+.1, ph-.05, '$\iota^+$', **kwargs)
+    plt.text(ph+.1, -.05, r'$I^+$', **kwargs)
+    plt.text(-ph+.1, -ph-.1, '$\iota^-$', **kwargs)
+    plt.text(ph+.1, -ph-.05, '$\iota^0$', **kwargs)
+    # plt.plot(UV_range, ph * np.ones_like(UV_range), **kwargs)
+    # plt.plot(-ph*np.ones_like(UV_range), UV_range, **kwargs)
+    plt.gca().set_aspect('equal')
+    plt.legend()
 
 if __name__ == "__main__":
     from make_all_figures import plot_and_save
--- phd_courses/general_relativity_exercises/figures/kerr_infall.py
+++ phd_courses/general_relativity_exercises/figures/kerr_infall.py
@@ -1,30 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-r = np.linspace(2, 10, num=1000)
+def kerr_infall():
+    l = 0
+    a = .9
+    M = 1
 
-e = .1
-l = 0
-a = .5
-M = 1
+    r = np.linspace(M + np.sqrt(M**2 - a**2)+1e-2, 10, num=10000)
 
-theta = np.pi / 2
+    theta = np.pi / 2
 
-rhosq = r**2 + (a*np.cos(theta))**2
-delta = r**2 - 2*M*r + a**2
+    def metric_coeffs(r, theta=np.pi/2):
+        rhosq = r**2 + (a*np.cos(theta))**2
+        delta = r**2 - 2*M*r + a**2
+        g_tt = - (1 - 2 * M * r / rhosq)
+        g_tp = - 2 * M * a * r * np.sin(theta)**2 / rhosq
+        g_pp = (r**2 + a**2 + 2 * M * r * a**2 * np.sin(theta)**2 / rhosq) * np.sin(theta)**2
+        g_rr = rhosq / delta
+        
+        return g_tt, g_tp, g_pp, g_rr
 
-g_tt = - (1 - 2 * M * r / rhosq)
-g_tp = - 2 * M * a * r * np.sin(theta)**2 / rhosq
-g_pp = (r**2 + a**2 + 2 * M * r * a**2 * np.sin(theta)**2 / rhosq) * np.sin(theta)**2
-g_rr = rhosq / delta
+    g_tt, g_tp, g_pp, g_rr = metric_coeffs(r[-1])
+    e = np.sqrt(g_tp**2 / g_pp - g_tt)
 
-tdot = e / (g_tp**2 / g_pp - g_tt)
-pdot = - g_tp / g_tt * tdot
+    g_tt, g_tp, g_pp, g_rr = metric_coeffs(r)
 
-rdotsq = ( -1 + e * tdot) / g_rr
+    tdot = e / (g_tp**2 / g_pp - g_tt)
+    pdot = - g_tp / g_tt * tdot
 
-plt.plot(r, tdot, label=r'$\dot{t}$')
-plt.plot(r, pdot, label=r'$\dot{\varphi}$')
-plt.plot(r, rdotsq, label=r'$|\dot{r}|^2$')
-plt.legend()
-plt.show()
+    rdotsq = ( -1 + e * tdot) / g_rr
+
+    plt.semilogy(r, tdot, label=r'$\dot{t}$')
+    # plt.semilogy(r, abs(pdot) / tdot, label=r'$\dot{\varphi} / \dot{t}$')
+    plt.semilogy(r, np.sqrt(rdotsq), label=r'$\dot{r}$')
+    
+    tau = (np.cumsum(np.sqrt(rdotsq)[::-1]) * np.gradient(r))[::-1]
+    plt.semilogy(r, tau, label=r'$\tau$ (proper time from $r_0$)')
+    
+    plt.ylabel('Various quantities')
+    plt.xlabel('$r$ [units of $M$]')
+    plt.legend()
+
+if __name__ == "__main__":
+    from make_all_figures import plot_and_save
+    plot_and_save(kerr_infall)
